1d85b9a53c897fd1e2ef19191f45db85218faef2
Dan Wells <danwells396@gmail.com>
2022-05-31 15:10:56 +0100

    Add notes about getting Waveglow inference to work

--- job_scripts/A4-fastpitch-inference.sh
+++ job_scripts/A4-fastpitch-inference.sh
@@ -3,7 +3,7 @@
 # grid engine options
 #$ -N fastpitch_infer
 #$ -l h_rt=01:00:00
-#$ -l h_vmem=16G
+#$ -l h_vmem=32G
 #$ -pe gpu-titanx 1
 #$ -o /exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/UUN_Firstname_Lastname/$JOB_NAME_$JOB_ID.stdout
 #$ -e /exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/UUN_Firstname_Lastname/$JOB_NAME_$JOB_ID.stderr
@@ -28,22 +28,30 @@ DS_HOME=/exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/${UUN}_${YOUR_NAM
 FP=$DS_HOME/FastPitches/PyTorch/SpeechSynthesis/FastPitch
 
 # see $FP/scripts/download_{fastpitch,waveglow}.sh to get pre-trained
-# checkpoints, or substitute your own
-export FASTPITCH="$FP/pretrained_models/fastpitch/nvidia_fastpitch_210824.pt"
+# checkpoints, or substitute your own (can pass as script argument)
+export FASTPITCH="${1:-$FP/pretrained_models/fastpitch/nvidia_fastpitch_210824.pt}"
 export WAVEGLOW="$FP/pretrained_models/waveglow/nvidia_waveglow256pyt_fp16.pt"
+
+# NB. at least the waveglow checkpoint tries to deserialise straight to
+# CUDA device, so need to run this in a GPU environment even if running
+# inference on CPU afterwards!
 
 export PHRASES="$FP/phrases/devset10.tsv"
 export OUTPUT_DIR=$DS_HOME/fastpitch_audio/$(basename ${PHRASES} .tsv)
-export BATCH_SIZE=32  # this might need to be bigger than #utts in $PHRASES...
+export BATCH_SIZE=16  # this might need to be bigger than #utts in $PHRASES...
 
 # these affect model architecture => match to settings used during model training!
-export PHONE=true
+export PHONE=true  # PHONE=true seems not to respect --p-arpabet?
 export ENERGY=true
 export NUM_SPEAKERS=1
 export SPEAKER=0  # select speaker by index
 
-export CPU=false  # false => run on GPU
+# I had trouble running this on GPU, something about the CUDA memory
+# allocation was off. Running on CPU works, but is quite slow (~15 minutes for
+# 10 utterances) and still needs a lot of RAM (16 GB is not enough, 32 GB is,
+# don't know in between)
+export CPU=true  # false => run on GPU
 export AMP=false
 
 cd $FP
-scripts/inference.sh
+scripts/inference_example.sh
